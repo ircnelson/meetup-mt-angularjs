0c78de3a60f378c86fb8c4a431a62d70572418b0
Nelson Junior <ircnelson@gmail.com>
2015-06-08 02:05:13 +0000

    retirado acesso global ao metodo de controller

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,22 +1,15 @@
 /*
-    criação de um módulo dentro do Angular
+    o escopo de código abaixo vai permanecer dentro da função e não terá acesso global
 */
-var app = angular.module('meetup', []);
-
-/*
-    registrando o controller dentro do módulo
-*/
-app.controller('MainController', MainController);
-
-function MainController ($scope) {
-    $scope.soma = function (n, m) {
-        return n + ' + ' + m + ' = ' + (n + m);
-    };
-}
-
-/*
-    dica: cuidado com o escopo do javascript. :-)
+(function () {
+    var app = angular.module('meetup', []);
     
-          do jeito que está o MainController está acessível de forma global... isso é realmente necessário?
-          vamos melhorar isso...
-*/
+    app.controller('MainController', MainController);
+    
+    function MainController ($scope) {
+        $scope.soma = function (n, m) {
+            return n + ' + ' + m + ' = ' + (n + m);
+        };
+    }
+
+})();
